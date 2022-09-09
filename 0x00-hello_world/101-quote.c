0914fea8093ac00e3892719c5a398eab846b48c2
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-09 07:11:36 -0700

    qoute

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - Prints output
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\"-Dora korpar, 2015-10-19\n",59);
+	return (1);
+}
