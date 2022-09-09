17f7ea01d8dfab2b384479e3c307cabae9e9f00d
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-09 04:54:13 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Prints output
+ *
+ * Return:Always 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("size of a char:%d byte(s)\n",sizeof(a));
+	printf("size of an int:%d byte(s)\n",sizeof(b));
+	printf("size of a long int:%d byte(s)\n",sizeof(c));
+	printf("size of a long long int:%d byte(s)\n",sizeof(d));
+	printf("size of a float:%d byte(s)\n",sizeof(e);
+	return (0);
+}
