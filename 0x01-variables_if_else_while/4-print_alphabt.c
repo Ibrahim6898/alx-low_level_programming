6559f29e838b6a0bc433e529e099d07358092eea
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-09 13:59:00 -0700

    alphab

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,11 @@
 
 int main(void)
 {
-	char c, e, q;
-	e = 'e';
-	q = 'q';
+	char c;
 	
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		if (c != 'e' && c != q)
-			putchar(c);
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
