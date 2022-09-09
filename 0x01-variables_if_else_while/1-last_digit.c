82e5657e283b7cf626e9e358fb329a96b7cebfe8
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-09 10:13:10 -0700

    last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,12 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <time.h> 
-  
- /** 
-  *   * main - Entry point
-  *
-  *  Return: Always 0
-  */  
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
 int main(void) 
 { 
 	int n; 
