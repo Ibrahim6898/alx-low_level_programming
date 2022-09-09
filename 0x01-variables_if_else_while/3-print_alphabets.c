50b023110299a8af143b705f531c62278a0b9485
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-09 13:38:52 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,13 @@ int main(void)
 {
 	char c;
 
-	for(c = 'a'; c <= 'z'; c++)
-	{
-		if(c != 'e' && c != 'q')
+	for (c = 'a'; c <= 'z'; c++)
 		{
 			putchar(c);
 		}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
