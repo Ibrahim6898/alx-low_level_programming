457740cd28d0a2c8bcb17bb5ddfe6077589398b4
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-09 04:56:24 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("size of an int:%d byte(s)\n",sizeof(b));
 	printf("size of a long int:%d byte(s)\n",sizeof(c));
 	printf("size of a long long int:%d byte(s)\n",sizeof(d));
-	printf("size of a float:%d byte(s)\n",sizeof(e);
+	printf("size of a float:%d byte(s)\n",sizeof(e));
 	return (0);
 }
