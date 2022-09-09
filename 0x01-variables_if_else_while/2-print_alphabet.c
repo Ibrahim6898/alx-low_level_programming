0ab726398402682d6e32097c37477bca79b2c289
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-09 10:36:35 -0700

    printalpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <='z'; low++)
+		putchar(low);
+		putchat(\n');
+	return (0);
+}
