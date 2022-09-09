69c0a9093ba04508cd83b987e7baefe632ce1185
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-09 01:58:01 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printing
+ *
+ * Return: always0(successful)
+ */
+int main(void) 
+{
+	puts("Programming is like building a multilingual puzzle\n");
+       	return (0);
+}
