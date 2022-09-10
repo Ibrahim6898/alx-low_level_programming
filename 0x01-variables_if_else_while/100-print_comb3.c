12e1e8288b6c4022f07adb01dc873a98ae9d6d75
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-10 16:07:06 -0700

    printcom3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,20 +3,20 @@
  * main - Entry point
  *
  * Description: using main function
- * this program prints "All possible different combinations
- * Return 0
+ * this program prints "All possible different combinations of 2 digits
+ * Return: 0
  */
 int main(void)
 {
 	int c1, c2;
 
-	for (c1 = 0; c1 <= 89; c1++)
+	for (c1 = 39; c1 <= 48; c1++)
 	{
-		for (c2 = 0; c2 <= 89; c2++)
+		for (c2 = 39; c2 <= 48; c2++)
 		{
 			putchar(c1);
 			putchar(c2);
-			if (c1 != 89 || c2 != 89)
+			if (c1 != 48 || c2 != 48)
 			{
 			putchar(',');
 			putchar(' ');
