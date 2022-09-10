c6b4eb61676318eb8d72c97b42023d87eaac35c5
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-10 11:18:30 -0700

    printebalpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero or negative
+ * Return: 0
+ */
+
+int main(0)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
