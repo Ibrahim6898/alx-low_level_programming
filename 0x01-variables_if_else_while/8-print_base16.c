970206cf0b67efa31095c6177b77ff1d10ea440a
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-10 11:29:28 -0700

    printbase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,15 +2,15 @@
 /**
  * main - Entry point
  *
- * Description: this program prints "Programming is positivw, zero or negative
+ * Description: using the main function
+ * this program prints "Programming is positivw, zero or negative
  * Return: 0
  */
-
 int main(void)
 {
 	char c;
 
-	for (c = '0'; c<= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 	putchar(c);
 	}
