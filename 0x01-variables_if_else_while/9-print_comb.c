7c404df800616ff3123f59ff7f531e8c434c4939
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-10 11:48:45 -0700

    printcomb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-int c;
+	int c;
 
-for (c = 48; c <= 57; c++)
-{
-putchar(c);
-if (c != 57)
-{
-putchar(',');
-putchar('');
-}
-}
-putchar('\n');
-return (0);
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
