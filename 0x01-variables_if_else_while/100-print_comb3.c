c134398465b1d62bae8109f26d9c2339f3cc26ee
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-10 15:29:56 -0700

    printcom3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: using main function
+ * this program prints "All possible different combinations
+ * Return 0
+ */
+int main(void)
+{
+	int c1, c2;
+
+	for (c1 = 0; c1 <= 10; c++)
+	{
+		for (c2 = 0; c2 <= 0; c2++)
+		{
+			putchar ((c1 % 10) + '0');
+			putchar ((c2 % 10) + '0');
+			if (c1 ==  9 && c2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+	}
+	}
