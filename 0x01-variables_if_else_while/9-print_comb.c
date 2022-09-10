0cb622e01c3933f3a10e4f47f9f0a545491923ce
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-10 12:23:57 -0700

    printcomb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,20 +4,16 @@
  *
  * Description: using the main function
  * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
- * Return:0
+ * Return: 0
  */
 int main(void)
 {
-	int c;
+	int c = 0;
 
-	for (c = 0; c <= 9; c++)
+	for (c = 10; c <= 99; c++)
 	{
-	putchar(c + '0');
-	if (c < 9)
-	{
+	putchar(c);
 	putchar(',');
-	putchar('');
-	}
 	}
 	putchar('\n');
 	return (0);
