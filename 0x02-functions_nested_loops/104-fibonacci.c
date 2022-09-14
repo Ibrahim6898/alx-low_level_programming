9935d72f13410ef893418085a4bde34fe4e8bd89
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-14 16:03:15 -0700

    104fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
-	umsigned long int i, j, k, j1, j2, k1, k2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
 	j = 1;
 	k = 2;
 
-	printf("%lu", j)
+	printf("%lu", j);
 	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", k);
