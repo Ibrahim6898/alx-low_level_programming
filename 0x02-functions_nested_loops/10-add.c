1da076bc5b20e455721fbdd01e5f1aa667892261
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-14 09:04:44 -0700

    numadd

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,10 @@
 /**
  * add - Adds two integers
  * @num1: The first integer to be added.
- * The second integer to be added.
+ * @num2: The second integer to be added.
  * Return: The result of the addition.
  */
 int add(int num1, int num2)
 {
-	return(num1 + num2);
+	return (num1 + num2);
 }
