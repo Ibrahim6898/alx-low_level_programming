f53b2769568c2af5370230d8c009816959d7cbf7
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-19 13:55:05 -0700

    pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,17 +1,34 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
 
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-	      
-#endif
+#define MAIN_H
+
+
+
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char *str);
+
+#endif /*MAIN_H*/
