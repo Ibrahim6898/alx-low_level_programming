e9f25233e10582a4bb8b7560f9b93c9405c19ee3
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-19 16:57:59 -0700

    morenested

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,28 +4,30 @@
 
 /**
  *print_square - prints squares
- * *@size: parameter
+ *@size: parameter
  *Return: returns nothing
  */
 
+
 void print_square(int size)
+
 {
 	int inc1, inc2;
 
 	if (size > 0)
 	{
 		for (inc1 = 0; inc1 < size; inc1++)
-
-		for (inc2 = 0; inc2 < (size - 1); inc2++)
-	{
-		putchar('#');
+		{
+			for (inc2 = 0; inc2 < (size - 1); inc2++)
+			{
+				putchar('#');
+			}
+			putchar('#');
+			putchar('\n');
+		}
 	}
-		putchar('#');
+	else
+	{
 		putchar('\n');
 	}
-}
-else
-{
-	puchar('\n');
-}
-}
+
