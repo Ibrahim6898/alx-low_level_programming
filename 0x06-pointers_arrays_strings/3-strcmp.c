5a96e6171c4d98089e7cc352582cc4eb9b83b626
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-22 04:20:38 -0700

    motepointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  _strcmp - Function which compare two strings and
+ *_strcmp - Function which compare two strings and
  *@s1: first string
  *@s2:second string
  *Return:
@@ -24,7 +24,8 @@ int _strcmp(char *s1, char *s2)
 				break;
 			}
 			else if (s2[i] == '\0')
-			{diff = s1[i];
+			{
+				diff = s1[i];
 				break;
 			}
 			else if (s1[i] != s2[i])
@@ -33,7 +34,7 @@ int _strcmp(char *s1, char *s2)
 				break;
 			}
 			else
-				i++
+				i++;
 		}
 		return (diff);
 }
