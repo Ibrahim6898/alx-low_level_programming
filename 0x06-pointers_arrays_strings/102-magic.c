697f2a5276bf4042ba19ae3afa09a35aa4b3a9f7
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-22 20:36:20 -0700

    pointers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -17,7 +17,7 @@ int main(void)
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
-	/* ...sothat this prints prints 98\n */
+	/* ...so that this prints 98\n */
 	printf("a[12] = %d\n", a[2]);
 	return (0);
 }
