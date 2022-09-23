96ccdc95dc581bfe6614fc25052ca2c96e668a21
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-09-23 04:31:05 -0700

    pointers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,22 +7,16 @@
 
 void print_number(int n)
 {
-
 	unsigned int num = n;
 
 	if (n < 0)
 	{
-
 		_putchar('-');
-
 		num = -num;
 	}
-	if (num > 9)
+	if ((num / 10) > 0)
 	{
-
 		print_number(num / 10);
-
 	}
 	_putchar(num % 10 + '0');
-
 }
