d777f36acc600038c1e479e3a54975042eae5d30
ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-10-10 15:28:42 +0100

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
-/*
- * main - prints the name of the file
+/**
+ * main - prints the file
  *
- * Return: Always 0;
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("%s\n", __FILE__);'
+	printf("%s\n", __FILE__);
 	return (0);
 }
