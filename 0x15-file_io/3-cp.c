8b5e6bfa2c2fabd159982c765db2c32c4aa2809b
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-11-01 08:52:46 -0700

    copy_file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,29 +41,29 @@ int main(int ac, char **av)
 
 	op1 = open(av[1], O_RDONLY);
 	if (op1 == -1)
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	op2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (op2 == -1)
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
 	while ((rd = read(op1, reads, 1024)) > 0)
 	{
 		reads[rd] = '\0';
-		wr = write(op2, reads, rd);
+
+	wr = write(op2, reads, rd);
 		if (wr != rd)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 		}
-		if (rd == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-
-			cl1 = close(op1);
-			if (cl1 == -1)
-			close_error(op1);
-			cl2 = close(op2);
-			if (cl2 == -1)
-				close_error(op2);
-				return (0);
 	}
+	if (rd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	cl1 = close(op1);
+	if (cl1 == -1)
+		close_error(op1);
+	cl2 = close(op2);
+	if (cl2 == -1)
+		close_error(op2);
+	return (0);
 }
