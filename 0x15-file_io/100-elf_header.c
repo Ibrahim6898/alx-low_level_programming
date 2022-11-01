3de9c5dbae9d3bf13c0e8fc5e9706e8e8a109234
Ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2022-11-01 08:31:48 -0700

    elf_header

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point to the elf program
+ * @ac: count of the number of command line argument
+ * @av: the argument array
+ *
+ * Return: 0 (sucess)
+ */
+
+int main(int ac, char **av)
+{
+
+		return (0);
+}
